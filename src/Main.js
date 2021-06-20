b38f2c2b26af8de0f4575c8080d01a1640dbe96e
abu-nofal <anofal719@gmail.com>
2021-06-20 18:00:52 +0300

    done

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import HornedBeast from './HornedBeast' 
+import HornedBeast1 from './HornedBeast1'
+
+ class Main extends Component {
+  render() {
+    return (
+      <div>
+        <HornedBeast/>
+        <HornedBeast1/>
+        
+      </div>
+    )
+  }
+}
+
+export default Main
